cccb77499ce483f1c110da982ab2a85a6dda7dfa
Lukas <wenning.lukas@web.de>
2024-05-11 20:03:19 +0200

    begin ui

--- lua/nvim-todo/ui.lua
+++ lua/nvim-todo/ui.lua
@@ -2,16 +2,28 @@ local popup = require('plenary.popup')
 
 function ShowMenu()
     local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-    local win_id = popup.create({}, {
-        title = 'todo',
-        lines = vim.o.lines / 2,
-        col = vim.o.columns / 2,
-        minwidth = 60,
-        minheight = 30,
+    local height = 20
+    local width = 60
+    local picker_win_id = popup.create({}, {
+        title = 'Todo list',
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
         borderchars = borderchars,
         cb = function (_, sel)
             print("it works")
         end
     }) 
-
+    --local bufnr = vim.api.nvim_win_get_buf(win_id)
+    --vim.api.nvim_buf_set_keymap(bufnr, 'n', 'q', '<cmd>lua CloseMenu()<CR>', {silent = false})
+    local prompt_win_id = popup.create({}, {
+        title = 'Add a Todo',
+        line = math.floor(((vim.o.lines - height) / 2) + 22),
+        col = math.floor((vim.o.columns - width) / 2),
+        minheight = 1,
+        minwidth = 60,
+        borderchars = borderchars
+    })
 end
+
